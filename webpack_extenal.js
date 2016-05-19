2668a0040c4e008716379bacd46cec9c22b1c8b7
linxixuan <zmx.morgan@gmail.com>
2016-05-19 15:46:40 +0800

    自定义external

---
+++ webpack_extenal.js
@@ -0,0 +1,11 @@
+var externals = [{
+    'react': {
+      amd: 'react',
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react'
+    },
+    'react-dom': 'ReactDOM'
+}];
+
+module.exports = externals;
