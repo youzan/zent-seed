f583f7a1109a5a27ffd49863a864703bcc79298f
linxixuan <zmx.morgan@gmail.com>
2016-05-24 15:14:56 +0800

    使用完整的config的配置文件，更大的自由度

---
+++ zent.wepback.config.js
@@ -0,0 +1,13 @@
+var conf = {
+    externals: [{
+        'react': {
+          amd: 'react',
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react'
+        },
+        'react-dom': 'ReactDOM'
+    }]
+};
+
+module.exports = conf;
