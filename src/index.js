54b168f7ed71dbcd6c71c3901a904b7c76b6ad1f
Li Chen <lichen@youzan.com>
2016-08-23 17:06:03 +0800

    update felint config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Seed extends Component {
   render() {
-    return (<div className="zent-title">seed</div>)
+    return (<div className="zent-title">seed</div>);
   }
 }
